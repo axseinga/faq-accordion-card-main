0d0365fad0bf7f0a54d75ff6865a60446165ca4f
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-03-13 12:35:23 +0000

    add functionality

---
+++ js.js
@@ -0,0 +1,43 @@
+// all functionality working, need to find way to not repeat the code, loop
+
+const btn = document.querySelector(".btn--1");
+const text = document.querySelector(".text--1");
+
+btn.addEventListener("click", function () {
+  text.classList.toggle("hidden");
+});
+
+const btn2 = document.querySelector(".btn--2");
+const text2 = document.querySelector(".text--2");
+
+btn2.addEventListener("click", function () {
+  text2.classList.toggle("hidden");
+});
+
+const btn3 = document.querySelector(".btn--3");
+const text3 = document.querySelector(".text--3");
+
+btn3.addEventListener("click", function () {
+  text3.classList.toggle("hidden");
+});
+
+const btn4 = document.querySelector(".btn--4");
+const text4 = document.querySelector(".text--4");
+
+btn4.addEventListener("click", function () {
+  text4.classList.toggle("hidden");
+});
+
+const btn5 = document.querySelector(".btn--5");
+const text5 = document.querySelector(".text--5");
+
+btn5.addEventListener("click", function () {
+  text5.classList.toggle("hidden");
+});
+
+const btn6 = document.querySelector(".btn--6");
+const text6 = document.querySelector(".text--6");
+
+btn6.addEventListener("click", function () {
+  text6.classList.toggle("hidden");
+});
