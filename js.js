22072ee966867a6afc6ee5be5fc0cbc24742ef8b
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-04-14 12:29:32 +0100

    add js features

--- js.js
+++ js.js
@@ -1,43 +1,40 @@
-// all functionality working, need to find way to not repeat the code, loop
+const btn = document.querySelectorAll(".btn");
+const text = document.querySelectorAll(".text");
+const que = document.querySelectorAll(".card__text__box__li__que--h");
+const arrow = document.querySelectorAll(".arrow");
 
-const btn = document.querySelector(".btn--1");
-const text = document.querySelector(".text--1");
-
-btn.addEventListener("click", function () {
-  text.classList.toggle("hidden");
+btn[0].addEventListener("click", function () {
+  text[0].classList.toggle("hidden");
+  que[0].classList.toggle("bold");
+  arrow[0].classList.toggle("rotate");
 });
 
-const btn2 = document.querySelector(".btn--2");
-const text2 = document.querySelector(".text--2");
-
-btn2.addEventListener("click", function () {
-  text2.classList.toggle("hidden");
+btn[1].addEventListener("click", function () {
+  text[1].classList.toggle("hidden");
+  que[1].classList.toggle("bold");
+  arrow[1].classList.toggle("rotate");
 });
 
-const btn3 = document.querySelector(".btn--3");
-const text3 = document.querySelector(".text--3");
-
-btn3.addEventListener("click", function () {
-  text3.classList.toggle("hidden");
+btn[2].addEventListener("click", function () {
+  text[2].classList.toggle("hidden");
+  que[2].classList.toggle("bold");
+  arrow[2].classList.toggle("rotate");
 });
 
-const btn4 = document.querySelector(".btn--4");
-const text4 = document.querySelector(".text--4");
-
-btn4.addEventListener("click", function () {
-  text4.classList.toggle("hidden");
+btn[3].addEventListener("click", function () {
+  text[3].classList.toggle("hidden");
+  que[3].classList.toggle("bold");
+  arrow[3].classList.toggle("rotate");
 });
 
-const btn5 = document.querySelector(".btn--5");
-const text5 = document.querySelector(".text--5");
-
-btn5.addEventListener("click", function () {
-  text5.classList.toggle("hidden");
+btn[4].addEventListener("click", function () {
+  text[4].classList.toggle("hidden");
+  que[4].classList.toggle("bold");
+  arrow[4].classList.toggle("rotate");
 });
 
-const btn6 = document.querySelector(".btn--6");
-const text6 = document.querySelector(".text--6");
-
-btn6.addEventListener("click", function () {
-  text6.classList.toggle("hidden");
+btn[5].addEventListener("click", function () {
+  text[5].classList.toggle("hidden");
+  que[5].classList.toggle("bold");
+  arrow[5].classList.toggle("rotate");
 });
